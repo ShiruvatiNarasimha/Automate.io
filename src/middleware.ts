a40169102af3139752e7678f2e2539c84010554c
Shiruvati Narasimha <mrnattyboy05@gmail.com>
2024-10-20 04:27:58 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,21 @@
+
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const publicRoutes = createRouteMatcher([
-  "/",
-  "/api/clerk-webhook",
-  "/api/drive-activity/notification",
-  "/api/payment/success",
-]);
+  '/',
+  '/signin(.)',
+  '/singup(.)',
+  '/api/clerk-webhook',
+  '/api/drive-activity/notification',
+])
 
 const ignoredRoutes = createRouteMatcher([
-  "/api/auth/callback/discord",
-  "/api/auth/callback/notion",
-  "/api/auth/callback/slack",
-  "/api/flow",
-  "/api/cron/wait",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  // Restrict admin routes to users with specific permissions
-  if (ignoredRoutes(req)) {
-    auth().protect((has) => {
-      return (
-        has({ permission: "org:sys_memberships:manage" }) ||
-        has({ permission: "org:sys_domains_manage" })
-      );
-    });
-  }
-  // Restrict organization routes to signed in users
-  if (publicRoutes(req)) auth().protect();
-});
-
+  '/api/auth/callback/discord',
+  '/api/auth/callback/notion',
+  '/api/auth/callback/slack',
+  '/api/flow',
+  '/api/cron/wait',
+])
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
