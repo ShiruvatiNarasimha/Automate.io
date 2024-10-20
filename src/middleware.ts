ff6251d653f1dae29b55603704ef767254ce0a1a
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-10-20 11:11:48 +0530

    updated Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,28 @@
-
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const publicRoutes = createRouteMatcher([
-  '/',
-  '/signin(.)',
-  '/singup(.)',
-  '/api/clerk-webhook',
-  '/api/drive-activity/notification',
-])
+  "/",
+  "/signin(.)",
+  "/singup(.)",
+  "/api/clerk-webhook",
+  "/api/drive-activity/notification",
+]);
 
 const ignoredRoutes = createRouteMatcher([
-  '/api/auth/callback/discord',
-  '/api/auth/callback/notion',
-  '/api/auth/callback/slack',
-  '/api/flow',
-  '/api/cron/wait',
-])
+  "/api/auth/callback/discord",
+  "/api/auth/callback/notion",
+  "/api/auth/callback/slack",
+  "/api/flow",
+  "/api/cron/wait",
+]);
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+// This logic means that all routes except publicRoutes and ignoredRoutes,  are protected ( i.e. Login is required )
+export default clerkMiddleware((auth, req) => {
+  if (!(publicRoutes(req) || ignoredRoutes(req))) {
+    auth().protect();
+  }
+});
