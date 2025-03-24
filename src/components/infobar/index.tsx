94fd7ac37b96e7fd6cfdad9ff13af2bc1a130b03
Shiruvati <mrnattyboy05@gmail.com>
2025-03-24 18:47:05 +0530

    Update index.tsx

--- src/components/infobar/index.tsx
+++ src/components/infobar/index.tsx
@@ -26,9 +26,6 @@ const InfoBar = (props: Props) => {
   //     }
   //   }
 
-  //   useEffect(() => {
-  //     onGetPayment()
-  //   }, [])
 
   return (
     <div className="flex flex-row justify-end gap-6 items-center px-4 py-4 w-full dark:bg-black ">
